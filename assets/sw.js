7f6ed68a66622fee7113ec47327c6d190882312d
Jer <92110462+Saxopwned@users.noreply.github.com>
2025-01-13 11:25:51 -0500

    configure import

--- assets/sw.js
+++ assets/sw.js
@@ -2,8 +2,8 @@ var cacheName = 'egui-template-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './eframe_template.js',
-  './eframe_template_bg.wasm',
+  './warframe_relic_finder.js',
+  './warframe_relic_finder_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
